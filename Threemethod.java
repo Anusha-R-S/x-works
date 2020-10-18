0f86fb62dc1c0397498326954a74dd4594dd70b3
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-18 19:02:09 +0530

    4th commit

---
+++ Threemethod.java
@@ -0,0 +1,16 @@
+class Aish
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Aishu");
+		music();
+		drinking();
+		
+	}
+	public static void music(){
+		System.out.println("music");
+	}
+	public static void drinking(){
+		System.out.println("drinking");
+	}
+}
