6c7ab4dd207b231abcf9f02d0ad27f4c23acbff2
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-18 19:00:48 +0530

    added my third commit

---
+++ Fruit.java
@@ -0,0 +1,33 @@
+class FruitJuice
+{
+public static void main(String[] args)
+{
+System.out.println("Fruits;");
+Apple();
+ORANGE();
+Banana();
+Blueberry();
+Strawberry();
+}
+public static void Apple()
+{
+System.out.println("Apple");
+}
+public static void ORANGE()
+{
+System.out.println("ORANGE");
+}
+public static void Banana()
+{
+System.out.println("Banana");
+}
+public static void Blueberry()
+{
+System.out.println("Blueberry");
+}
+public static void Strawberry()
+{
+System.out.println("Strawberry");
+}
+}
+
