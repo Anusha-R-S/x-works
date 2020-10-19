567b23483eeaa87fb115acaa31487123ede094ee
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-19 21:42:25 +0530

    Add files via upload

---
+++ DatatypeExample3.java
@@ -0,0 +1,52 @@
+public class DatatypeExample3
+{
+public static void main(String[] args)
+{
+   byte membersOfCricket;
+   short initialSalary;
+   int costOfBugatti;
+   long typesOfAnimals;
+   float speedOfUsainBolt;
+   double weightOfEarth;
+   boolean AgricultureIsImportant;
+   char alphabet;
+   String rule;    
+   System.out.println("Data types with examples:");
+   members();
+   salary();
+   bugatti();
+   types();
+   usinBolt();
+   weight();
+   agriculture();
+   alphabet();
+   rule();
+}
+public static void members(){
+	System.out.println("Total Members Of Cricket ARE 11");
+}
+public static void salary(){
+	System.out.println("Initial salary of engineer is 30000 ");
+}
+public static void bugatti(){
+	System.out.println("cost of bugatti is 41 crores");
+}
+public static void types(){
+	System.out.println("world contains large number of types animals in world");
+}
+public static void usinBolt(){
+	System.out.println(" speed Of Usain Bolt is 43.99 kilometers per hour (27.33 miles per hour) ");
+}
+public static void weight(){
+	System.out.println("Weght of earth is 5.972 × 10^24kg  ");
+}
+public static void agriculture(){
+	System.out.println("Agriculture Is Important -TRUE");
+}
+public static void alphabet(){
+	System.out.println("we can assign alphabets uniquely");
+}
+public static void rule(){
+	System.out.println("There are several rules which should be followed while driving  ");
+}
+}
