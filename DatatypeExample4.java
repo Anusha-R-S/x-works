1289c1315ec9c3d56ba54a79c7e5ca9e8aa58e1a
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-19 21:43:06 +0530

    Add files via upload

---
+++ DatatypeExample4.java
@@ -0,0 +1,52 @@
+public class DatatypeExample4
+{
+public static void main(String[] args)
+{
+   byte numKouravas;
+   short heightOfMountEverest;
+   int netWorthOfGoogle;
+   long speciesOfAquaticAnimals;
+   float gdp;
+   double labReadings;
+   boolean cowGivesMilk;
+   char index;
+   String ramayan;    
+   System.out.println("Data types with examples:");
+   kourava();
+   mount();
+   google();
+   aqua();
+   gdp();
+   lab();
+   cow();
+   index();
+   ramayan();
+}
+public static void kourava(){
+	System.out.println("there were total 100 kouravas");
+}
+public static void mount(){
+	System.out.println("height Of Mount Everest 29,035 feet");
+}
+public static void google(){
+	System.out.println("net Worth Of Google is $822,471,200.");
+}
+public static void aqua(){
+	System.out.println("species Of Aquatic Animals are billion in number");
+}
+public static void gdp(){
+	System.out.println("gdp is unpredictable ");
+}
+public static void lab(){
+	System.out.println("lab Readings are predictable");
+}
+public static void cow(){
+	System.out.println("cow gives milk-true");
+}
+public static void index(){
+	System.out.println("we assign alphabets in index");
+}
+public static void ramayan(){
+	System.out.println("ramayan is story of ram and seetha");
+}
+}
