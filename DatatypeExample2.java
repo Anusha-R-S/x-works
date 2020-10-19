91e9ca186254f2e5dd77b0c9eda88425a303db72
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-19 21:41:21 +0530

    Add files via upload

---
+++ DatatypeExample2.java
@@ -0,0 +1,53 @@
+public class DatatypeExample2
+{
+public static void main(String[] args)
+{
+   byte teamsInIpl;
+   short priceOfTv;
+   int numberOfRivers;
+   long numberOfStars;
+   float height;
+   double distanceBetweenEarthAndSun;
+   boolean chocolateIsSweet;
+   char classDivisions;
+   String pets;    
+   System.out.println("Data types with examples:");
+   teams();
+   price();
+   rivers();
+   stars();
+   height();
+   distance();
+   sweet();
+   division();
+   pets();
+}
+public static void teams(){
+	System.out.println("There are 12 teams in IPL");
+}
+public static void price(){
+	System.out.println("Price of television is 30000 ");
+}
+public static void rivers(){
+	System.out.println("There are millions of rivers in the world ");
+}
+public static void stars(){
+	System.out.println("Number of Stars are in trillions or more than that");
+}
+public static void height(){
+	System.out.println("Height of Aishwarya is 5.4 ");
+}
+public static void distance(){
+	System.out.println("Distance between earth and sun is 148.98 million km");
+}
+public static void sweet(){
+	System.out.println("Chocolate Is a type of Sweet");
+}
+public static void division()
+{
+	System.out.println("Our departent have 5 divisions as A,B,C,D,E");
+}
+public static void pets(){
+	System.out.println("I have 2 pets");
+}
+}
