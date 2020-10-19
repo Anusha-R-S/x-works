d3b66018a53f30050a316096869e2aea269dbb66
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-19 21:39:02 +0530

    Add files via upload

---
+++ DatatypeExample1.java
@@ -0,0 +1,52 @@
+public class DatatypeExample1
+{
+public static void main(String[] args)
+{
+   byte age;
+   short daysInAYear;
+   int numberOfEngineersInIndia;
+   long populationOfIndia;
+   float percentageOfStudent;
+   double chargeOfElectron;
+   boolean earthIsFlat;
+   char grade;
+   String name;    
+   System.out.println("Data types with examples:");
+   age();
+   days();
+   engineers();
+   popultion();
+   percenatge();
+   electron();
+   earth();
+   grade();
+   name();
+}
+public static void age(){
+	System.out.println("Age of any animals on this planet is variable");
+}
+public static void days(){
+	System.out.println("Days are in a leap year are 366 ");
+}
+public static void engineers(){
+	System.out.println("Number of engineers in India per YEAR are 10,00,000 ");
+}
+public static void popultion(){
+	System.out.println("India contains 2nd largest population in world");
+}
+public static void percenatge(){
+	System.out.println("Knowledge of a person shouldnot be measured by his/her degree percenatge ");
+}
+public static void electron(){
+	System.out.println("Charge of electron is 1.60217662 *10^-19");
+}
+public static void earth(){
+	System.out.println("Earth is not flat");
+}
+public static void grade(){
+	System.out.println("Students are assigned with particular grades according to their marks");
+}
+public static void name(){
+	System.out.println("My name is Anusha");
+}
+}
