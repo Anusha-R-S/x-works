ca730d2493b9e8f7ff38af30b74ea8eb375ed1dd
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-20 19:19:40 +0530

    finalKeywordEx

---
+++ ExampleFinal.java
@@ -0,0 +1,11 @@
+public class ExampleFinal{
+public static void main(String[] A){
+final String adharnumber="819851195656";
+System.out.println("adhar number is "+adharnumber);
+final String email;
+email="asulibhavimath@gmail.com";
+System.out.println("email id is "+email);
+final String phoneNumber="7892148367";
+System.out.println("phone number is "+phoneNumber);
+}
+}
