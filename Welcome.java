6109677ccaf6fd3d9155d0d567f7e552ba0621d4
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-20 19:24:47 +0530

    welcomeEx

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome{
+public static void main(String[] args){
+System.out.println("invoked main");
+printwish("welcome");
+}
+public static void printwish(String printwish){
+	System.out.println("Hello "+printwish);
+} 
+
+} 
