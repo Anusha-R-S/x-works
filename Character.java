80b09f378e511a0a74b6da586f484ba2ba8187c8
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-21 20:03:56 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,54 @@
+public class Character{
+	public static void main(String[] args){
+		System.out.println("characters");
+		charOfArray();
+	}
+	public static void charOfArray(){
+		char[] array= new char[10];
+		array[0]='a';
+		array[1]='b';
+		array[2]='c';
+		array[3]='d';
+		array[4]='e';
+		array[5]='f';
+		array[6]='g';
+		array[7]='h';
+		array[8]='i';
+	    char elementAtIndex1=array[0];
+		 
+		 System.out.println("Element at index 1 "+elementAtIndex1);
+		 
+		 char elementAtIndex2=array[1];
+		 
+		 System.out.println("Element at index 2 "+elementAtIndex2);
+		 
+		 char elementAtIndex3=array[2];
+		 
+		 System.out.println("Element at index 3 "+elementAtIndex3);
+		 char elementAtIndex4=array[3];
+		 
+		 System.out.println("Element at index 4 "+elementAtIndex4);
+		 char elementAtIndex5=array[4];
+		 
+		 System.out.println("Element at index 5 "+elementAtIndex5);
+		 char elementAtIndex6=array[5];
+		 
+		 System.out.println("Element at index 6 "+elementAtIndex6);
+		 char elementAtIndex7=array[6];
+		 
+		 System.out.println("Element at index 7 "+elementAtIndex7);
+		char elementAtIndex8=array[7];
+		 
+		 System.out.println("Element at index 8 "+elementAtIndex8);
+		 char elementAtIndex9=array[8];
+		 
+		 System.out.println("Element at index 9 "+elementAtIndex9);
+		char elementAtIndex10=array[9];
+		 
+		 
+	
+	}
+	
+	
+	
+}
