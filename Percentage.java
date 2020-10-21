52020d8922c57fc48f35a96c3f6ac4d4af610324
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-21 19:56:45 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,26 @@
+public class Percentage
+{
+public static void main(String[] args)
+{
+System.out.println("Percentages: ");
+arrayOfPercentage();
+}
+public static void arrayOfPercentage()
+{
+System.out.println("invoking arrayOfPercentage");
+double sslcPerc=73.12;
+double pucPerc=60;
+double degPerc=6;
+double[] percentages={sslcPerc,pucPerc,degPerc};
+int sizeOfArray=percentages.length;
+System.out.println("All elements "+sizeOfArray);
+double elementAtIndex0=percentages[0];
+System.out.println("Element at index two "+elementAtIndex0);
+
+double elementAtIndex1=percentages[1];
+System.out.println("Element at index one "+elementAtIndex1);
+
+double elementAtIndex2=percentages[2];
+System.out.println("Element at index three "+elementAtIndex2);
+}
+}
