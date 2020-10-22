db2d296dc041b3ee117a513a4d9f9cbda3827b96
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-22 19:35:50 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,34 @@
+//Pass least 15 state names as argument to main method and print on console
+public class States
+{
+	public static void main(String[] state)
+	{
+		System.out.println("India have: ");
+		
+		String statesArray="States";
+		statesList("Karnataka");
+		statesList("Punjab");
+		statesList("Kerala");
+		statesList("Maharastra");
+		statesList("Gujarat");
+		statesList("Delhi");
+		statesList("Tamilnadu");
+		statesList("Nagaland");
+		statesList("Uttar Pradesh");
+		statesList("Assam");
+		statesList("Jammu & Kashmir");
+		statesList("Bihar");
+		statesList("Andhra Pradesh");
+		statesList("Rajastan");
+		statesList("Odisha");
+		
+	}
+	public static void statesList(String name)
+	{
+		System.out.println("States are "+ name );		
+		
+		
+		
+		
+	}
+}
