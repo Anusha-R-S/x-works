79cc4f532d430071d5b25598d1eb7636b660f679
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-10-27 22:59:53 +0530

    Add files via upload

---
+++ AgeChecker.java
@@ -0,0 +1,13 @@
+public class AgeChecker
+{
+	public static void main(String[] test)
+	{
+		System.out.println("Invoked by JVM");
+		
+		String CheckAge=Ages.ageFinder(3);
+		
+		System.out.println("the age format is:" +CheckAge);
+	
+	System.out.println("exit: main");
+	}
+}
