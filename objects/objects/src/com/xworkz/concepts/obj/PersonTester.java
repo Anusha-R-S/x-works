d29e8d8bb5ceeff29f2fd89cd9f801ede4b4caa3
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-11-23 21:12:02 +0530

    Add files via upload

---
+++ objects/objects/src/com/xworkz/concepts/obj/PersonTester.java
@@ -0,0 +1,21 @@
+package com.xworkz.concepts.obj;
+
+public class PersonTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person.name="Yash";
+		Person.dob="08/01/1986";
+		Person.address="Basavangudi";
+		
+		Person.displayInfo();
+		
+		Person.name="Alia";
+		//Person.dob="31/08/1993";
+		//Person.address="Mumbai";
+		
+		Person.displayInfo();
+
+	}
+
+}
